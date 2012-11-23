a9e333399e690239c8ed66f84af91ce469ad9d15
kevinpapst <kpapst@gmx.net>
2012-11-23 23:31:34 +0100

    initial import from subversion

---
+++ jquery/multiselect/0.4/js/locale/ui-multiselect-de.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale en, en-US
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'Alle hinzufügen',
+	removeAll:'Alle entfernen',
+	itemsCount:'Einträge ausgewählt'
+});
